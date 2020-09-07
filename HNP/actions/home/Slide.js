7bc010c7fce4c1cd9f15b1a414ca750cfb8b9a42
catalin.rosu <d9f10ee5c4267b63b9801077a96e4f379057133a>
2020-09-08 00:47:58 +0300

    add video to index and service page

--- HNP/actions/home/Slide.js
+++ HNP/actions/home/Slide.js
@@ -1,69 +1,74 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let data = [
-        {
-            img: "https://www.hnp.ro/wp-content/uploads/2020/08/W_T_Turning_Perform_1920-1080_RO-900x500.jpg",
-            titleWithe: "Gama",
-            titleYellow: "Walter Perform,",
-            description: "Etalon pentru fiabilitate i eficienta econimica.",
-            id: 1
-        },
-        {
-            id: 2,
-            img: "https://www.hnp.ro/wp-content/uploads/2020/08/W_All_Perform_1920-1080_RO-900x500.jpg",
-            titleWithe: "Building Up",
-            titleYellow: "Vision,<br/>  Leading Future",
-            description: "We offer the most complete industrial solutions in the country, for the research, design and development of materials.",
-        },
-        {
-            id: 3,
-            img: "https://www.hnp.ro/wp-content/uploads/2020/06/promotie1_smicat_HNP-e1597263209992-900x500.png",
-            titleWithe: "Building Up",
-            titleYellow: "Vision,<br/>  Leading Future",
-            description: "We offer the most complete industrial solutions in the country, for the research, design and development of materials.",
-        }
+document.addEventListener("DOMContentLoaded", function () {
+  let data = [
+    {
+      img:
+        "https://www.hnp.ro/wp-content/uploads/2020/08/W_T_Turning_Perform_1920-1080_RO-900x500.jpg",
+      titleWithe: "Gama",
+      titleYellow: "Walter Perform,",
+      description: "Etalon pentru fiabilitate i eficienta econimica.",
+      id: 1,
+    },
+    {
+      id: 2,
+      img:
+        "https://www.hnp.ro/wp-content/uploads/2020/08/W_All_Perform_1920-1080_RO-900x500.jpg",
+      titleWithe: "Building Up",
+      titleYellow: "Vision,<br/>  Leading Future",
+      description:
+        "We offer the most complete industrial solutions in the country, for the research, design and development of materials.",
+    },
+    {
+      id: 3,
+      img:
+        "https://www.hnp.ro/wp-content/uploads/2020/06/promotie1_smicat_HNP-e1597263209992-900x500.png",
+      titleWithe: "Building Up",
+      titleYellow: "Vision,<br/>  Leading Future",
+      description:
+        "We offer the most complete industrial solutions in the country, for the research, design and development of materials.",
+    },
+  ];
+  let startSlide = "";
 
-    ];
-    let startSlide = "";
+  data.forEach(function (data) {
+    startSlide +=
+      '<div class="item home-slider-bg" id="imgSlide-' + data.id + '">';
+    startSlide += '   <div class="container">';
+    startSlide += '      <div class="slider-dtl top-effect">';
+    startSlide +=
+      '          <h1 class="slider-heading">' +
+      data.titleWithe +
+      " <span>" +
+      data.titleYellow +
+      "</span> </h1>";
+    startSlide +=
+      '          <h5 class="slider-sub-heading">' + data.description + " </h5>";
+    startSlide +=
+      '          <a href="#" class="btn btn-default">Contact Us </a>';
+    startSlide += "      </div>";
+    startSlide += "   </div>";
+    startSlide += "</div>";
+  });
 
-    data.forEach(function (data) {
-        startSlide += '<div class="item home-slider-bg" id="imgSlide-'+data.id+'">';
-        startSlide += '   <div class="container">';
-        startSlide += '      <div class="slider-dtl top-effect">';
-        startSlide += '          <h1 class="slider-heading">' + data.titleWithe + ' <span>' + data.titleYellow + '</span> </h1>';
-        startSlide += '          <h5 class="slider-sub-heading">' + data.description + ' </h5>';
-        startSlide += '          <a href="#" class="btn btn-default">Contact Us </a>';
-        startSlide += '      </div>';
-        startSlide += '   </div>';
-        startSlide += '</div>';
-    });
+  let containerId = document.getElementById("home-slider");
+  //containerId.innerHTML = startSlide;
+  data.forEach(function (data) {
+    //  document.getElementById("imgSlide-"+data.id).style.backgroundImage = "url('"+data.img+"')"
+  });
 
-    let containerId = document.getElementById("home-slider");
-    containerId.innerHTML = startSlide;
-    data.forEach(function (data) {
-        document.getElementById("imgSlide-"+data.id).style.backgroundImage = "url('"+data.img+"')"
-    });
-
-    $('head').append(' <link href="css/owl.carousel.css" rel="stylesheet" type="text/css"/> ');
-        $.ajax({
-            url: 'js/owl.carousel.js',
-            dataType: 'script',
-            success: function () {
-                $.ajax({
-                    url: 'js/theme.js',
-                    dataType: 'script',
-                    success: function () {
-                    },
-                    async: true
-                });
-            },
-            async: true
-        });
-
-
-
+  $("head").append(
+    ' <link href="css/owl.carousel.css" rel="stylesheet" type="text/css"/> '
+  );
+  $.ajax({
+    url: "js/owl.carousel.js",
+    dataType: "script",
+    success: function () {
+      $.ajax({
+        url: "js/theme.js",
+        dataType: "script",
+        success: function () {},
+        async: true,
+      });
+    },
+    async: true,
+  });
 });
-
-
-
-
-
--- HNP/commun/NavBar.js
+++ HNP/commun/NavBar.js
@@ -1,106 +1,110 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let location = window.location.href.split('/');
-    let home = '<li><a href="index.html">ACASA</a>';
-    let produse = '<li><a href="produse.html">PRODUSE</a>';
-    let produse_detail = '<li><a href="#">ACASA</a>';
-    switch (location[location.length - 1]) {
-        case "index.html" :
-            home = '<li class="active"><a href="index.html">ACASA</a>';
-            break;
-        case "produse.html" :
-            produse = '<li class="active"><a href="produse.html">PRODUSE</a>';
-            break;
-        case "produse-details.html" :
-            produse_detail = '<li class="active"><a href="#">Detalii</a>';
-            break;
+document.addEventListener("DOMContentLoaded", function () {
+  let location = window.location.href.split("/");
+  let home = '<li><a href="index.html">ACASA</a>';
+  let produse = '<li><a href="produse.html">PRODUSE</a>';
+  let produse_detail = '<li><a href="#">ACASA</a>';
+  switch (location[location.length - 1]) {
+    case "index.html":
+      home = '<li class="active"><a href="index.html">ACASA</a>';
+      break;
+    case "produse.html":
+      produse = '<li class="active"><a href="produse.html">PRODUSE</a>';
+      break;
+    case "produse-details.html":
+      produse_detail = '<li class="active"><a href="#">Detalii</a>';
+      break;
+  }
+  let navBar =
+    '<div class="container">' +
+    '        <div class="row">' +
+    '            <div class="col-sm-4">' +
+    '                <div class="logo">' +
+    '                    <a href="index.html"><img src="HNP_IMG/HNP_logoXS.png" alt="logo"></a>' +
+    "                </div>" +
+    "            </div>" +
+    '            <div class="col-sm-8">' +
+    '                <div class="row">' +
+    '                    <div class="col-sm-3">' +
+    '                        <div class="info-nav">' +
+    '                            <div class="info-nav-heading">Phone Number</div>' +
+    '                            <div class="info-nav-dtl"><a href="tel:+40 744 774 500">+40 744 774 500</a></div>' +
+    "                        </div>" +
+    "                    </div>" +
+    '                    <div class="col-sm-3">' +
+    '                        <div class="info-nav">' +
+    '                            <div class="info-nav-heading">Email Address:</div>' +
+    '                            <div class="info-nav-dtl"><a href="mailto:office@hnp.ro">office@hnp.ro</a></div>' +
+    "                        </div>" +
+    "                    </div>" +
+    '                    <div class="col-sm-3">' +
+    '                        <div class="info-nav">' +
+    '                            <div class="info-nav-heading">Address:</div>' +
+    '                            <div class="info-nav-dtl">300714 Timișoara,<br> Bd. Industriilor, nr. 5</div>' +
+    "                        </div>" +
+    "                    </div>" +
+    "                </div>" +
+    "            </div>" +
+    '            <div class="col-sm-10 pad-0">' +
+    '                <div class="navigation">' +
+    '                    <div id="cssmenu">' +
+    "                        <ul>" +
+    home +
+    "</li>" +
+    produse +
+    " </li>" +
+    '                            <li><a href="#">SERVICII</a>' +
+    "                            </li>" +
+    '                            <li><a href="#">NOUTATI</a>' +
+    "                            </li>" +
+    '                            <li><a href="#">CONTACT</a>' +
+    "                            </li>" +
+    "                        </ul>" +
+    "                    </div>" +
+    "                </div>" +
+    "            </div>" +
+    "        </div>" +
+    "        <!-- search -->" +
+    '        <div class="search">' +
+    '            <div class="container">' +
+    '                <input type="search" class="search-box" placeholder="Search"/>' +
+    '                <a href="#" class="fa fa-times search-close"></a>' +
+    "            </div>" +
+    "        </div>" +
+    "        <!-- end search -->" +
+    "        <!-- request btn -->" +
+    '        <div class="modal fade request-model" id="request-model" role="dialog">' +
+    '            <div class="modal-dialog" role="document">' +
+    '                <div class="modal-content">' +
+    '                    <div class="modal-header">' +
+    '                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span' +
+    '                                aria-hidden="true">&times;</span></button>' +
+    '                        <h5 class="modal-title text-center">Request A Quote</h5>' +
+    "                    </div>" +
+    '                    <div class="modal-body request-model-body text-center">' +
+    "                        <form>" +
+    '                            <div class="form-group">' +
+    '                                <input type="text" class="form-control" id="name" placeholder="Enter Your Name">' +
+    "                            </div>" +
+    '                            <div class="form-group">' +
+    '                                <input type="email" class="form-control" id="email" placeholder="Enter Your Email">' +
+    "                            </div>" +
+    '                            <div class="form-group">' +
+    '                                <input type="text" class="form-control" id="subject" placeholder="Enter Subject">' +
+    "                            </div>" +
+    '                            <div class="form-group">' +
+    '                                <textarea id="message" name="message" rows="6" placeholder="Message"></textarea>' +
+    "                            </div>" +
+    '                            <button type="submit" class="btn btn-default">Submit Request</button>' +
+    "                        </form>" +
+    "                    </div>" +
+    "                </div>" +
+    "            </div>" +
+    "        </div>" +
+    "        <!-- end request btn -->" +
+    "    </div>";
 
-    }
-    let navBar =
-        "<div class=\"container\">" +
-        "        <div class=\"row\">" +
-        "            <div class=\"col-sm-4\">" +
-        "                <div class=\"logo\">" +
-        "                    <a href=\"index.html\"><img src=\"HNP_IMG/HNP_logoXS.png\" alt=\"logo\"></a>" +
-        "                </div>" +
-        "            </div>" +
-        "            <div class=\"col-sm-8\">" +
-        "                <div class=\"row\">" +
-        "                    <div class=\"col-sm-3\">" +
-        "                        <div class=\"info-nav\">" +
-        "                            <div class=\"info-nav-heading\">Phone Number</div>" +
-        "                            <div class=\"info-nav-dtl\"><a href=\"tel:+40 744 774 500\">+40 744 774 500</a></div>" +
-        "                        </div>" +
-        "                    </div>" +
-        "                    <div class=\"col-sm-3\">" +
-        "                        <div class=\"info-nav\">" +
-        "                            <div class=\"info-nav-heading\">Email Address:</div>" +
-        "                            <div class=\"info-nav-dtl\"><a href=\"mailto:office@hnp.ro\">office@hnp.ro</a></div>" +
-        "                        </div>" +
-        "                    </div>" +
-        "                    <div class=\"col-sm-3\">" +
-        "                        <div class=\"info-nav\">" +
-        "                            <div class=\"info-nav-heading\">Address:</div>" +
-        "                            <div class=\"info-nav-dtl\">300714 Timișoara,<br> Bd. Industriilor, nr. 5</div>" +
-        "                        </div>" +
-        "                    </div>" +
-        "                </div>" +
-        "            </div>" +
-        "            <div class=\"col-sm-10 pad-0\">" +
-        "                <div class=\"navigation\">" +
-        "                    <div id=\"cssmenu\">" +
-        "                        <ul>" +
-                                     home + "</li>" +
-                                     produse + " </li>" +
-        "                            <li><a href=\"#\">SERVICII</a>" +
-        "                            </li>" +
-        "                            <li><a href=\"#\">NOUTATI</a>" +
-        "                            </li>" +
-        "                            <li><a href=\"#\">CONTACT</a>" +
-        "                            </li>" +
-        "                        </ul>" +
-        "                    </div>" +
-        "                </div>" +
-        "            </div>" +
-        "        </div>" +
-        "        <!-- search -->" +
-        "        <div class=\"search\">" +
-        "            <div class=\"container\">" +
-        "                <input type=\"search\" class=\"search-box\" placeholder=\"Search\"/>" +
-        "                <a href=\"#\" class=\"fa fa-times search-close\"></a>" +
-        "            </div>" +
-        "        </div>" +
-        "        <!-- end search -->" +
-        "        <!-- request btn -->" +
-        "        <div class=\"modal fade request-model\" id=\"request-model\" role=\"dialog\">" +
-        "            <div class=\"modal-dialog\" role=\"document\">" +
-        "                <div class=\"modal-content\">" +
-        "                    <div class=\"modal-header\">" +
-        "                        <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\"><span" +
-        "                                aria-hidden=\"true\">&times;</span></button>" +
-        "                        <h5 class=\"modal-title text-center\">Request A Quote</h5>" +
-        "                    </div>" +
-        "                    <div class=\"modal-body request-model-body text-center\">" +
-        "                        <form>" +
-        "                            <div class=\"form-group\">" +
-        "                                <input type=\"text\" class=\"form-control\" id=\"name\" placeholder=\"Enter Your Name\">" +
-        "                            </div>" +
-        "                            <div class=\"form-group\">" +
-        "                                <input type=\"email\" class=\"form-control\" id=\"email\" placeholder=\"Enter Your Email\">" +
-        "                            </div>" +
-        "                            <div class=\"form-group\">" +
-        "                                <input type=\"text\" class=\"form-control\" id=\"subject\" placeholder=\"Enter Subject\">" +
-        "                            </div>" +
-        "                            <div class=\"form-group\">" +
-        "                                <textarea id=\"message\" name=\"message\" rows=\"6\" placeholder=\"Message\"></textarea>" +
-        "                            </div>" +
-        "                            <button type=\"submit\" class=\"btn btn-default\">Submit Request</button>" +
-        "                        </form>" +
-        "                    </div>" +
-        "                </div>" +
-        "            </div>" +
-        "        </div>" +
-        "        <!-- end request btn -->" +
-        "    </div>";
-    var containerId = document.getElementById("nav-bar");
-    containerId.innerHTML = navBar;
+  var containerId = document.getElementById("nav-bar");
+  hidePreloader();
+  containerId.innerHTML = navBar;
+  //  $("video").prop("muted", false);
 });
